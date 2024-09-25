72f7c32ea969411f8fac6db86bc9071470242aed
SoloBaChi <solobachi02@gmail.com>
2024-09-25 03:23:26 +0100

    additional user model fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,55 +1,64 @@
 const mongoose = require("mongoose");
 
-const { Schema , model} = mongoose;
+const { Schema, model } = mongoose;
 
-const UserSchema = new Schema({
- fullName:{
-    type:String,
-    required:true
- },
- email:{
-    type:String,
-    unique:true,
-    required:true
- },
- password:{
-    type:String,
-    required:true,
- },
- confirmPassword:{
-    type:String,
-    required:true,
- },
- phoneNumber:{
-    type:Number,
-    required:true
- },
- profile: {
-    type: String,
-    default: "",
+const UserSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    department: {
+      type: String,
+    },
+    level: {
+      type: String,
+      default: "100",
+    },
+    profile: {
+      type: String,
+      default: "",
+    },
+    noOfContributions: {
+      type: Number,
+      default: 0,
+    },
+    activationToken: {
+      type: String,
+    },
+    authCode: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
   },
-  noOfContributions:{
-  contributions:[]
-  },
-  activationToken: {
-    type: String,
-  },
-  authCode: {
-    type: String,
-  },
-  isActive: {
-    type: Boolean,
-    default: false,
-  },
-},
-{
+  {
     timestamps: true,
     // include virtual properties in the JSON representation of the document.
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-}
-)
+  },
+);
 
-const userModel =  model("user",UserSchema);
+const userModel = model("user", UserSchema);
 
 module.exports = userModel;
