fac34a6f076dbaed4cb8ce259afa963e4026e0bb
SoloBaChi <solobachi02@gmail.com>
2024-09-28 19:28:13 +0100

    changed reset password routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,9 @@ const {
   signUp,
   login,
   activateUser,
+  confirmResetPassword,
+  verifyResetPasswordToken,
+  sendResetPassowrdToken,
 } = require("../controllers/auth.controller");
 const { body } = require("express-validator");
 
@@ -92,6 +95,55 @@ body("password")
 
 
 /** AcTIVATE A USER */
-router.get("/activate",activateUser)
+router.get("/activate",activateUser);
+
+
+
+// FORGOT PASSWORD  ROUTES
+router.post(
+  "/reset-token",
+  body("email").isEmail().withMessage("Please enter a valid email address"),
+  sendResetPassowrdToken,
+);
+
+router.put(
+  "/verify-token",
+  body("otp")
+  .isString()
+  .isLength({
+    min: 4,
+    max: 4,
+  })
+  .withMessage(
+      `Please enter the correct four digit code sent to your email`,
+    ),
+  verifyResetPasswordToken,
+);
+
+router.put(
+  "/reset-password",
+  body("newPassword")
+  .isStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minSymbols: 1,
+    minLowercase: 1,
+  })
+  .withMessage(
+    `Password must be 8 characters and should include a number,symbol and an uppercase`,
+  ),
+  body("confirmNewPassword")
+  .isStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minSymbols: 1,
+    minLowercase: 1,
+  })
+  .withMessage(
+    `Password must be 8 characters and should include a number,symbol and an uppercase`,
+  ),
+  confirmResetPassword,
+);
+
 
 module.exports = router;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,14 +1,9 @@
 
 const router = require("express").Router();
 const {
-  sendResetPassowrdToken,
-  verifyResetPasswordToken,
   updateUser,
   getUser,
-  signUp,
-  login,
   deleteUsers,
-  confirmResetPassword,
 } = require("../controllers/auth.controller");
 const { body } = require("express-validator");
 // const protect = require("../middlewares/auth.middleware");
@@ -29,11 +24,7 @@ router.get("/my-profile", getUser);
 
 
 // POST METHOD
-router.post(
-  "/reset-token",
-  body("email").isEmail().withMessage("Please enter a valid email address"),
-  sendResetPassowrdToken,
-);
+
 
 
 
@@ -42,44 +33,6 @@ router.post(
 ///////////////////////
 //**PUT METHODS */
 router.put("/updateuser",updateUser);
-router.put(
-  "/verify-token",
-  body("otp")
-  .isString()
-  .isLength({
-    min: 4,
-    max: 4,
-  })
-  .withMessage(
-      `Please enter the correct four digit code sent to your email`,
-    ),
-  verifyResetPasswordToken,
-);
-
-router.put(
-  "/reset-password",
-  body("newPassword")
-  .isStrongPassword({
-    minLength: 8,
-    minUppercase: 1,
-    minSymbols: 1,
-    minLowercase: 1,
-  })
-  .withMessage(
-    `Password must be 8 characters and should include a number,symbol and an uppercase`,
-  ),
-  body("confirmNewPassword")
-  .isStrongPassword({
-    minLength: 8,
-    minUppercase: 1,
-    minSymbols: 1,
-    minLowercase: 1,
-  })
-  .withMessage(
-    `Password must be 8 characters and should include a number,symbol and an uppercase`,
-  ),
-  confirmResetPassword,
-);
 
 
 
