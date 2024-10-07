0822eadce85bbe077c42e7381e3cff2b849d9603
SoloBaChi <solobachi02@gmail.com>
2024-10-07 02:54:19 +0100

    added update password controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,7 @@ const {
   getUser,
   deleteUsers,
   deleteUser,
+  updateUserPassword,
 } = require("../controllers/auth.controller");
 const { body } = require("express-validator");
 // const protect = require("../middlewares/auth.middleware");
@@ -34,6 +35,38 @@ router.get("/my-profile", getUser);
 ///////////////////////
 //**PUT METHODS */
 router.put("/edit-user",updateUser);
+router.put("/update-password",
+ body("oldPassword")
+.isStrongPassword({
+  minLength: 8,
+  minUppercase: 1,
+  minSymbols: 1,
+  minLowercase: 1,
+})
+.withMessage(
+  `Old Password must be 8 characters and should include numbers,symbols and uppercase`,
+),
+  body("newPassword")
+.isStrongPassword({
+  minLength: 8,
+  minUppercase: 1,
+  minSymbols: 1,
+  minLowercase: 1,
+})
+.withMessage(
+  `New Password must be 8 characters and should include numbers,symbols and uppercase`,
+),
+body("confirmPassword")
+.isStrongPassword({
+  minLength: 8,
+  minUppercase: 1,
+  minSymbols: 1,
+  minLowercase: 1,
+})
+.withMessage(
+  `Confirm Password must be 8 characters and should include numbers,symbols and uppercase`
+),
+  updateUserPassword)
 
 
 
