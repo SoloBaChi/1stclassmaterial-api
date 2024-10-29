4b9e3ffd0bc2ab886582c04bb0bb7c0a10cd1a5e
SoloBaChi <solobachi02@gmail.com>
2024-10-29 01:06:05 +0100

    bug fix 2

--- src/controllers/contributor.controller.js
+++ src/controllers/contributor.controller.js
@@ -57,7 +57,7 @@ contributorController.createBook = async (req, res) => {
       courseCode,
       department,
       level,
-      docURL:`https://1stclassmaterial-api.vercel.app/materials/${encodeURIComponent(req.file.filename)}`,
+      docURL:`https://www.1stclassmaterial.com/materials/${encodeURIComponent(req.file.filename)}`,
       contributor: userId,
     });
 
